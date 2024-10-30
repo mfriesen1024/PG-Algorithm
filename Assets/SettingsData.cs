5c45c64974b30a6fc0cc478f8cfc1233cd55d79b
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2024-10-30 08:17:23 -0300

    Add some vars.

--- Assets/SettingsData.cs
+++ Assets/SettingsData.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace Assets
 {
-    internal class SettingsData
+    [Serializable]
+    public class SettingsData
     {
+        public float noiseScaleXY = 1;
+        public float noiseScaleZ = 8;
+        const float noiseVerticalDivisor = 256; // Divide vertical height of noise by this.
+
         public SettingsData()
         {
         }
