d262bae72eac4b9e150e5e41127d2a84779f385d
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2024-10-30 09:49:08 -0300

    Create chunkdata and a basic generator.

---
+++ Assets/ChunkData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using SimplexNoise;
+
+namespace Assets
+{
+    /// <summary>
+    /// Represents a region of terrain, so we don't load too much at once.
+    /// </summary>
+    internal class ChunkData:MonoBehaviour
+    {
+        Vector2 location;
+        SettingsData data;
+
+        Block[,] blocks;
+
+        public Vector2 GlobalLoc { get => location*data.chunkSize; }
+
+        public void Init(SettingsData data, Vector2 location)
+        {
+            this.location = location;
+
+            Generate(GlobalLoc);
+        }
+
+        void Generate(Vector2 offset)
+        {
+            // do proper octave generation later.
+            float[,] noiseValues = Noise.Calc2D(data.chunkSize, data.chunkSize, data.noiseScaleXY);
+            blocks = new Block[data.chunkSize, data.chunkSize];
+
+            for(int x = 0; x < noiseValues.GetLength(0); x++)
+            {
+                for(int z = 0; z < noiseValues.GetLength(1); z++)
+                {
+                    Block b = GameObject.CreatePrimitive(PrimitiveType.Cube).AddComponent<Block>();
+                    b.Type = BlockType.Grass;
+                    b.transform.position = new(x, noiseValues[x, z], z);
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            
+        }
+    }
+}
