7c58649d88ea242a643eb41054a065b2b9f9e5f1
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2024-10-30 08:49:59 -0300

    Add load distance.

--- Assets/SettingsData.cs
+++ Assets/SettingsData.cs
@@ -11,6 +11,7 @@ namespace Assets
     {
         public float noiseScaleXY = 1;
         public float noiseScaleZ = 8;
+        public int chunkLoadDist = 3; // How many chunks from the player
         const float noiseVerticalDivisor = 256; // Divide vertical height of noise by this.
 
         public SettingsData()
@@ -21,13 +22,15 @@ namespace Assets
         {
             noiseScaleXY = float.Parse(fileData[0]);
             noiseScaleZ = float.Parse(fileData[1]);
+            chunkLoadDist = int.Parse(fileData[2]);
         }
 
         internal string[] ToLines()
         {
             string[] data = {
             noiseScaleXY.ToString(),
-            noiseScaleZ.ToString()
+            noiseScaleZ.ToString(),
+            chunkLoadDist.ToString()
             };
             return data;
         }
