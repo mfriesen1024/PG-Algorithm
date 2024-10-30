2f3adb418bf900a6e9802bb9f1cf711167e03417
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2024-10-30 09:49:30 -0300

    Basic block stuff.

---
+++ Assets/Block.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets
+{
+    internal class Block : MonoBehaviour
+    {
+        public BlockType Type { get; internal set; }
+    }
+}
