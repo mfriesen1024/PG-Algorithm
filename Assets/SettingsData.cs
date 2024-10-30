fe9179914fa81ffe88e545ed1e69eadafe56d157
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2024-10-30 08:07:35 -0300

    Initial commit

---
+++ Assets/SettingsData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets
+{
+    internal class SettingsData
+    {
+        public SettingsData()
+        {
+        }
+
+        public SettingsData(string[] fileData)
+        {
+        }
+
+        internal string[] ToLines()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/TerrainGenerator.cs
@@ -0,0 +1,31 @@
+using Assets;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    SettingsData data;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Load();
+    }
+
+    private void Load()
+    {
+        string fName = "data\\settings.txt";
+        string dirName = "data";
+        Directory.CreateDirectory(dirName);
+        if (File.Exists(fName)) { data = new(File.ReadAllLines(fName)); }
+        else { data = new(); File.WriteAllLines(data.ToLines()); }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
