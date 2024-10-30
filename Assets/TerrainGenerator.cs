c815d3272e0cfce9b405e8d8ef43f02a414556d7
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2024-10-30 09:52:37 -0300

    Create initial generation setup.

--- Assets/TerrainGenerator.cs
+++ Assets/TerrainGenerator.cs
@@ -10,10 +10,21 @@ public class TerrainGenerator : MonoBehaviour
     [SerializeField] private SettingsData data;
     public SettingsData Data { get => data; private set => data = value; }
 
+    List<ChunkData> chunks = new();
+
     // Start is called before the first frame update
     void Start()
     {
         Load();
+        Generate();
+    }
+
+    private void Generate()
+    {
+        // For now, just generate one chunk with coords 0,0.
+        ChunkData cd = new GameObject().AddComponent<ChunkData>();
+        cd.Init(data, new(0, 0));
+        chunks.Add(cd);
     }
 
     private void Load()
