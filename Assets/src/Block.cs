aa98703056883578bffdc65712f69227d31d8f3d
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-30 21:36:35 -0300

    Add colouring to blocks.

--- Assets/src/Block.cs
+++ Assets/src/Block.cs
@@ -9,6 +9,19 @@ namespace Assets
 {
     internal class Block : MonoBehaviour
     {
-        public BlockType Type { get; internal set; }
+        private BlockType type;
+
+        public BlockType Type { get => type; set { type = value; OnSetType(); } }
+
+        private void OnSetType()
+        {
+            var renderer = GetComponent(typeof(MeshRenderer)) as MeshRenderer;
+
+            switch(type)
+            {
+                case BlockType.Dirt: renderer.material.color = Color.HSVToRGB(0.166f,0.75f,0.5f); break;
+                    case BlockType.Grass: renderer.material.color = Color.green; break;
+            }
+        }
     }
 }
