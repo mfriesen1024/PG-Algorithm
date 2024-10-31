0877125ee6a43aa2210c9444bf72a2fd9141bce8
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-10-30 21:36:44 -0300

    Add input stuff to cc

--- Assets/src/CameraController.cs
+++ Assets/src/CameraController.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Assets
 {
     [RequireComponent(typeof(Camera))]
-    internal class CameraController:MonoBehaviour
+    internal class CameraController : MonoBehaviour
     {
+        private void Update()
+        {
+            float delta = Time.deltaTime;
+            if (Input.GetKey(KeyCode.W)) { Move(Vector3.forward); }
+            if (Input.GetKey(KeyCode.A)) { Move(Vector3.left); }
+            if (Input.GetKey(KeyCode.S)) { Move(Vector3.back); }
+            if (Input.GetKey(KeyCode.D)) { Move(Vector3.right); }
 
+            void Move(Vector3 direction)
+            {
+                Vector3 newPos = transform.position + direction*delta*5;
+                transform.position = newPos;
+            }
+        }
     }
 }
