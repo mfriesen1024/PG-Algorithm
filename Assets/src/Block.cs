98820992407096aacce61f4631d0714ad859f003
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2024-11-06 11:53:38 -0400

    Do water things.

--- Assets/src/Block.cs
+++ Assets/src/Block.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
+﻿using UnityEngine;
 using Colour = UnityEngine.Color;
 
 namespace Assets
@@ -17,11 +12,28 @@ namespace Assets
         private void OnSetType()
         {
             var renderer = GetComponent(typeof(MeshRenderer)) as MeshRenderer;
+            Material standardShaderMaterial = renderer.material;
 
-            switch(type)
+            switch (type)
             {
-                case BlockType.Dirt: renderer.material.color = Colour.HSVToRGB(0.166f,0.75f,0.5f); break;
-                    case BlockType.Grass: renderer.material.color = Colour.green; break;
+                case BlockType.Dirt: renderer.material.color = Colour.HSVToRGB(0.166f, 0.75f, 0.5f); break;
+                case BlockType.Grass: renderer.material.color = Colour.green; break;
+                case BlockType.Water:
+                    Colour c = Colour.blue;
+                    c.a = 0.3f;
+                    renderer.material.color = c;
+
+                    // Yoinked this from somewhere to make it transparent.
+                    standardShaderMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                    standardShaderMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                    standardShaderMaterial.SetInt("_ZWrite", 0);
+                    standardShaderMaterial.DisableKeyword("_ALPHATEST_ON");
+                    standardShaderMaterial.DisableKeyword("_ALPHABLEND_ON");
+                    standardShaderMaterial.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+                    standardShaderMaterial.renderQueue = 3000;
+
+                    if (TryGetComponent(out BoxCollider b)) { b.enabled = false; }
+                    break;
             }
         }
     }
